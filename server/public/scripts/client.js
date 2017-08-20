d9567f77e8e0390f86dd3f847ed6eb294e3fcc48
Charles Renk <renkx008@umn.edu>
2017-08-20 17:11:49 -0500

    added server side get and post functions on server side. Still not implemented with angular on the dom.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,24 @@ app.controller('EmployeeController', ['$http', function ($http) {
     console.log('Employee Controller has been loaded');
     var self = this;
     self.employees = [];
+    self.getEmployees = function () {
+        $http({
+            method: 'GET',
+            url: '/employees'
+        }).then(function (response) {
+            console.log(response.data);
+            self.employees = response.data;
+        });
+    };
+    self.postNewEmployees = function () {
+        $http({
+            method: 'POST',
+            url: '/employees',
+            data: self.newEmployee
+        }).then(function (response) {
+            self.getEmployees();
+        })
+    }
+    self.getEmployees();
+
 }]);
--- server/routes/employees.js
+++ server/routes/employees.js
@@ -2,4 +2,46 @@ var express = require('express');
 var router = express.Router();
 var pool = require('../modules/pool');
 
+router.get('/', function(req, res) {
+	console.log('employees get was hit!');
+	pool.connect(function(errorConnectingToDatabase, client, done){
+		if(errorConnectingToDatabase) {
+			console.log('Error connecting to database', errorConnectingToDatabase);
+			res.sendStatus(500);
+		} else {
+			client.query('SELECT * FROM employees;', function(errorMakingQuery, result) {
+				done();
+				if(errorMakingQuery) {
+					console.log('Error making database query', errorMakingQuery);
+					res.sendStatus(500);
+				} else {
+					res.send(result.rows);
+				}
+			});
+		}
+	});
+});
+
+router.post('/', function(req, res){
+	console.log('employees post was hit!');
+	pool.connect(function(errorConnectingToDatabase, client, done){
+		if(errorConnectingToDatabase) {
+			console.log('Error connecting to database', errorConnectingToDatabase);
+			res.sendStatus(500);
+		} else {
+            client.query('INSERT INTO employees(first_name, last_name, job_title, annual_salary) VALUES ($1, $2, $3, $4);', 
+            [req.body.first_name, req.body.last_name, req.body.job_title, req.body.annual_salary],
+             function(errorMakingQuery, result) {
+				done();
+				if(errorMakingQuery) {
+					console.log('Error making database query', errorMakingQuery);
+					res.sendStatus(500);
+				} else {
+					res.sendStatus(201);
+				}
+			});
+		}
+	});
+});
+
 module.exports = router;
