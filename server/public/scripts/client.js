ef47089bd1f0404588c620e3bedffe932e14f1d2
Charles Renk <renkx008@umn.edu>
2017-08-21 08:37:48 -0500

    I fixed my concatenation issue with my total salary expenditures number. I used parseInt for both variables in my GET on the client side.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,13 +4,19 @@ app.controller('EmployeeController', ['$http', function ($http) {
     console.log('Employee Controller has been loaded');
     var self = this;
     self.employees = [];
+    self.total = 0;
     self.getEmployees = function () {
         $http({
             method: 'GET',
             url: '/employees'
         }).then(function (response) {
             console.log(response.data);
+            for(i = 0; i < response.data.length; i++){
+                var salary = response.data[i].annual_salary;
+                self.total = parseInt(self.total) + parseInt(salary);
+            };
             self.employees = response.data;
+            
         });
     };
     self.postNewEmployees = function () {
@@ -24,4 +30,5 @@ app.controller('EmployeeController', ['$http', function ($http) {
     }
     self.getEmployees();
 
+    self.total = '0'
 }]);
